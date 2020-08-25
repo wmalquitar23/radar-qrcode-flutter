ab5915de1ec8b3d74c8d1c192667052b75408a70
Randy Flores Jr <floresrandyjr@gmail.com>
2020-08-25 18:39:03 +0800

    Changed the information section into input field, changed the page into stateful widget, changed the structure of the build widget (#70)

--- lib/presentation/pages/user_details/user_details.page.dart
+++ lib/presentation/pages/user_details/user_details.page.dart
@@ -1,117 +1,191 @@
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:radar_qrcode_flutter/core/utils/color_util.dart';
 import 'package:radar_qrcode_flutter/presentation/widgets/buttons/primary_button_widget.dart';
-import 'package:radar_qrcode_flutter/presentation/widgets/containers/title_and_content_with_divider_wrapper.dart';
+import 'package:radar_qrcode_flutter/presentation/widgets/bar/custom_regular_app_bar.dart';
+import 'package:radar_qrcode_flutter/presentation/widgets/fields/custom_textfield.dart';
+import 'package:radar_qrcode_flutter/presentation/widgets/images/circle_image_widget.dart';
+import 'package:radar_qrcode_flutter/presentation/widgets/texts/description_text.dart';
+import 'package:radar_qrcode_flutter/presentation/widgets/texts/header_text.dart';
 
-class UserDetailsPage extends StatelessWidget {
-  const UserDetailsPage({Key key}) : super(key: key);
+class UserDetailsPage extends StatefulWidget {
+  @override
+  _UserDetailsPageState createState() => _UserDetailsPageState();
+}
+
+class _UserDetailsPageState extends State<UserDetailsPage> {
+  TextEditingController _addressController = TextEditingController();
+  TextEditingController _ageController = TextEditingController();
+
+  @override
+  void initState() {
+    _addressController.text = "Purok 123, Brgy 4, Bacolod Negros Occ.";
+    _ageController.text = "24";
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        centerTitle: true,
-        backgroundColor: Colors.white,
-        leading: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Icon(
-            Icons.arrow_back,
-            color: ColorUtil.primaryTextColor,
-          ),
-        ),
-        title: Text(
-          'User Details',
-          style: TextStyle(
-            color: Colors.black,
-          ),
-        ),
-      ),
-      backgroundColor: Colors.white,
-      body: Container(
-        margin: EdgeInsets.all(20),
-        child: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      image: AssetImage('assets/images/profile/lalisa.jpeg'),
-                      fit: BoxFit.cover)),
-              width: 155,
-              height: 155,
-            ),
-            SizedBox(height: 10),
-            Text(
-              'Fausto Lorem Macabantog',
-              style: Theme.of(context).textTheme.headline6.copyWith(
-                    color: ColorUtil.primaryColor,
-                  ),
-            ),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+      body: CustomRegularAppBar(
+        backgroundColor: Colors.transparent,
+        title: "User Details",
+        body: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
+                _buildImage(),
+                SizedBox(
+                  height: 10.0,
+                ),
                 Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      color: Colors.green,
-                    ),
-                  ),
-                  child: Icon(
-                    Icons.check,
-                    color: Colors.green,
-                    size: 10,
+                  child: HeaderText(
+                    title: "Lalisa Manoban",
+                    fontSize: 18,
+                    color: ColorUtil.primaryColor,
                   ),
                 ),
                 SizedBox(
-                  width: 5,
+                  height: 5.0,
                 ),
-                Text(
-                  'VERIFIED',
-                  style: Theme.of(context).textTheme.headline5,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      child: ExtendedImage.asset(
+                        'assets/images/undraw/success.png',
+                        width: 15,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5.0,
+                    ),
+                    DescriptionText(
+                      title: "VERIFIED",
+                      color: ColorUtil.primaryTextColor,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ],
                 ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                _buildAddressTextField(),
+                _buildAgeTextField(),
               ],
             ),
-            SizedBox(height: 50),
-            TitleAndContentWithDividerWrapper(
-                title: 'Address',
-                content:
-                    'Purok 123, Brgy 4 Ipsum 123, Bacolod Negros Occidental'),
-            TitleAndContentWithDividerWrapper(
-              title: 'Age',
-              content: '24',
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        elevation: 0.0,
+        color: Colors.transparent,
+        child: _buildDecisionButtons(),
+      ),
+    );
+  }
+
+  Widget _buildImage() {
+    return Center(
+      child: Container(
+        width: 120.0,
+        child: Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10.0),
+              child: CircleImage(
+                imageUrl: "assets/images/profile/lalisa.jpeg",
+                size: 120.0,
+                fromNetwork: false,
+              ),
             ),
-            Spacer(),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: PrimaryButton(
-                      text: 'APPROVE',
-                      color: Colors.green,
-                    ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: ColorUtil.primaryBackgroundColor,
+                    shape: BoxShape.circle),
+                child: Container(
+                  margin: EdgeInsets.all(5),
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                      color: Colors.black, shape: BoxShape.circle),
+                  child: Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                    size: 15,
                   ),
                 ),
-                Expanded(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: PrimaryButton(
-                      text: 'DECLINE',
-                      color: Colors.red,
-                    ),
-                  ),
-                )
-              ],
-            )
+              ),
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildAddressTextField() {
+    return CustomTextField(
+        label: "Address",
+        child: TextFormField(
+          controller: _addressController,
+          readOnly: true,
+          validator: (value) {
+            if (value.isEmpty) {
+              return "Empty field";
+            }
+            return null;
+          },
+          textInputAction: TextInputAction.next,
+          onFieldSubmitted: (v) => FocusScope.of(context).nextFocus(),
+        ));
+  }
+
+  Widget _buildAgeTextField() {
+    return CustomTextField(
+        label: "Age",
+        child: TextFormField(
+          controller: _ageController,
+          readOnly: true,
+          validator: (value) {
+            if (value.isEmpty) {
+              return "Empty field";
+            }
+            return null;
+          },
+          textInputAction: TextInputAction.next,
+          onFieldSubmitted: (v) => FocusScope.of(context).nextFocus(),
+        ));
+  }
+
+  Widget _buildDecisionButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: PrimaryButton(
+              text: 'APPROVE',
+              color: Colors.green,
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: PrimaryButton(
+              text: 'DECLINE',
+              color: Colors.red,
+            ),
+          ),
+        )
+      ],
     );
   }
 }
